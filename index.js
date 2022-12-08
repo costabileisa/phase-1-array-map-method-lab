aa20edf2993987f9e79dedc1994a46f0356d61b3
Isabella Costabile <costabileisa@gmail.com>
2022-12-08 12:10:28 -0500

    commit tree 82a91d8f0dc664ee912a517dd1673fff47caf882

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() { 
+  return tutorials.map(item => item.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" "))
 }
